c1345c56ac4eb8d32aa03154a1a1a2e6b6dbb590
jiavan <jiavan.com@gmail.com>
2019-01-21 00:34:03 +0800

    add maxial square

---
+++ 221-maximal-square.js
@@ -0,0 +1,34 @@
+/**
+ * @param {character[][]} matrix
+ * @return {number}
+ */
+var maximalSquare = function(matrix) {
+    // var res = 0;
+
+    // for (var i = 0; i < matrix.length; i++) {
+    //     for (var j = 0; j < matrix[i].length; j++) {
+    //         var max = 0;
+
+    //         if (matrix[i][j]) {
+    //             var k = 1;
+    //             while (matrix[i][j + k]) {
+    //                 var count = 0;
+    //                 for (var x = i; x <= i + k; x++) {
+    //                     for (var y = j; y <= j + k; y++) {
+    //                         if (matrix[x] && matrix[x][y]) {
+    //                             count++;
+    //                         }
+    //                     }
+    //                 }
+    //                 max = Math.pow(k + 1, 2);
+    //                 if (count === max) {
+    //                     k++;
+    //                 }
+    //             }
+    //             res = Math.max(max, res);
+    //         }
+    //     }
+    // } 
+
+    // return res;
+};
