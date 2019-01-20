560acea1e3c5b5bea8cec3db78781a22531a8834
jiavan <jiavan.com@gmail.com>
2019-01-21 00:32:32 +0800

    perf imporve

--- 015-3sum.js
+++ 015-3sum.js
@@ -24,31 +24,43 @@ var threeSum = function(nums) {
 
     // return [...res].map((item) => item.split(',').map(item => +item));
 
-    var j, k, target, res = [];
+    if (nums.length < 3) {
+        return [];
+    }
+
+    var res = [],
+        i = 0,
+        target,
+        sum,
+        j,
+        k;
 
     nums.sort((a, b) => a - b);
 
-    for (var i = 0; i < nums.length - 2; i++) {
+    for (; i < nums.length - 2; i++) {
         if (i === 0 || nums[i] > nums[i - 1]) {
-            target = 0 - nums[i];
+            target = -nums[i];
             j = i + 1;
             k = nums.length - 1;
 
             while (j < k) {
-                if (nums[j] + nums[k] === target) {
+                sum = nums[j] + nums[k];
+                if (sum === target) {
                     res.push([nums[i], nums[j], nums[k]]);
                     j++;
                     k--;
+
                     while (j < k && nums[j] === nums[j - 1]) {
                         j++;
                     }
+
                     while (j < k && nums[k] === nums[k + 1]) {
                         k--;
                     }
-                } else if (nums[j] + nums[k] < target) {
-                    j++;
-                } else {
+                } else if (sum > target) {
                     k--;
+                } else {
+                    j++;
                 }
             }
         }
